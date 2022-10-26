cae218acc6bb3f8d3f1a2e6c1275d757d67f83e6
unknown <LucasFeliciano02@outlook.com>
2022-10-26 09:16:32 -0300

    funções

--- contactform/views.py
+++ contactform/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def index(request):
@@ -10,18 +12,28 @@ def index(request):
         subject = request.POST.get('subject')
         message = request.POST.get('message')
         
-        data = {
-            'name': name, 
-            'email': email, 
-            'subject': subject, 
-            'message': message
-        }
-        message = '''
-        New message: {}
-        
-        From: {}
-        '''.format(data['message'], data['email'])
-        send_mail(data['subject'], message, 'emailRemetente@outlook.com', ['emailDestinatario@gmail.com'])
-        return HttpResponse('Thank you for submitting the form, we will be in touch soon')
-        
+        if email == "":
+            messages.add_message(request, constants.ERROR, 'Email inválido!')
+        else:        
+            try:
+                data = {
+                    'name': name, 
+                    'email': email, 
+                    'subject': subject, 
+                    'message': message
+                }
+                message = '''
+        Nova mensagem: 
+
+        {}
+                
+        From: EMAIL_REMETENTE
+                '''.format(data['message'], data['email'])
+                send_mail(data['subject'], message, 'EMAIL_REMETENTE', recipient_list=[email])
+                
+                messages.add_message(request, constants.SUCCESS, 'Email enviado com sucesso!')        
+                
+            except:
+                messages.add_message(request, constants.ERROR, 'Email inválido!')        
+            
     return render(request, 'contactform/index.html', {})
